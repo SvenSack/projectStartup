f2186b53c022c7956eb4409d0fa85c9b5b652909
Sven <Sven.Sackmann@web.de>
2020-02-13 14:49:58 +0100

    Started work on inventory system
    its a start ^^

---
+++ Project Startup/Assets/Assets/Scripts/InventCharButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventCharButton : MonoBehaviour
+{
+    private string name = "Please Senpai, I want a name UwU";
+    public int indexNumber;
+    private Sprite image;
+    private bool showDetails;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Set(string nam, int index, Sprite img)
+    {
+        name = nam;
+        indexNumber = index;
+        image = img;
+    }
+
+    public void ToggleDetails()
+    {
+        if (showDetails)
+        {
+            // show the additional stats
+        }
+        else
+        {
+            // hide the stats
+        }
+
+        showDetails = !showDetails;
+    }
+}
---
+++ Project Startup/Assets/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public List<Character> inventory = new List<Character>();
+    public GameObject[] possibleCharacters = new GameObject[6];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Character NewCharacter(int index)
+    {
+        GameObject newChar = Instantiate(possibleCharacters[index]);
+        Character character = newChar.GetComponent<Character>();
+        return character;
+    }
+}
