6be497770100ff060178ba52bfac2d0526d8fdaa
Avery <averyfukawa@gmail.com>
2020-02-18 22:00:11 +0100

    Music intro + loop, music mute, music volume slider

---
+++ Project Startup/Assets/Assets/Scripts/Audio/AudioSettings.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSettings : MonoBehaviour
+{
+    public AudioMixer musicMixer;
+
+    public bool muted = false;
+    public float currentVolume = 0.0f;
+    public void SetVolume(float volume)
+    {
+        if (!muted)
+        {
+            musicMixer.SetFloat("MusicVolume", volume);
+            currentVolume = volume;
+        }
+    }
+
+    public void SetMute()
+    {
+        if (muted)
+        {
+            // Enable the music
+            muted = false;
+            musicMixer.SetFloat("MusicVolume", currentVolume);
+        } else if (!muted)
+        {
+            // Disable the music
+            muted = true;
+            musicMixer.SetFloat("MusicVolume", -80.0f);
+        } 
+        
+        /*
+        if (muted)
+        {
+            muted = false;
+            musicMixer.SetFloat("MusicVolume", currentVolume);
+        }
+        */
+        
+    }
+}
