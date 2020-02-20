e789ad52a9cc89cf232afa37f36e6f764ec498e8
Avery <averyfukawa@gmail.com>
2020-02-20 23:35:56 +0100

    Loading music volume is set up, muted boolean is currently not working as intended
    When the boolean is SUPPOSED to be loaded as true, it flips to false, and as such "breaking" certain parts of the functionality like being able to hear music while the mute toggle is on
    
    i'm tired, i cannot figure it out

--- Project Startup/Assets/Assets/Scripts/Audio/AudioSettings.cs
+++ Project Startup/Assets/Assets/Scripts/Audio/AudioSettings.cs
@@ -1,14 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioSettings : MonoBehaviour
 {
     public AudioMixer musicMixer;
-
-    public bool muted = false;
+    public GameObject settingsMenu;
+    public Slider volumeSlider;
+    public Toggle muteToggle;
+    
+    public bool muted;
     public float currentVolume = 0.0f;
+    
+    private void Start()
+    {
+        Debug.Log("Current Muted bool value: " + muted);
+        // Load the previous audio level, and set the saved level on in the music mixer
+        currentVolume = PlayerPrefs.GetFloat("MusicVol");
+        musicMixer.SetFloat("MusicVolume", currentVolume);
+        volumeSlider.value = PlayerPrefs.GetFloat("SliderPos");
+        
+        Debug.Log(PlayerPrefs.GetInt("MutedBool") + " loaded MutedBool");
+        // Load whether it was muted bool
+        if (PlayerPrefs.GetInt("MutedBool") == 0)
+        {
+            muted = false;
+            muteToggle.isOn = false;
+            //musicMixer.SetFloat("MusicVolume", currentVolume);
+        } else if (PlayerPrefs.GetInt("MutedBool") == 1) 
+        {
+            muted = true;
+            muteToggle.isOn = true;
+            musicMixer.SetFloat("MusicVolume", -80.0f);
+        }
+    }
+    
+    private void Update()
+    {
+        Debug.Log("Current muted bool value: " + muted);
+        Debug.Log("saved MutedBool value: " + PlayerPrefs.GetInt("MutedBool"));
+        if (!enabled) return;
+        if (!muted)
+        {
+            PlayerPrefs.SetFloat("SliderPos", volumeSlider.value);
+            PlayerPrefs.SetFloat("MusicVol", currentVolume);
+        }
+        
+        if (muted == false)
+            PlayerPrefs.SetInt("MutedBool", 0);
+        else if (muted == true)
+            PlayerPrefs.SetInt("MutedBool", 1);
+        
+        //Debug.Log(PlayerPrefs.GetInt("MutedBool") + " Saved bool.\n0 is not muted, 1 is muted");
+    }
+    
     public void SetVolume(float volume)
     {
         if (!muted)
@@ -33,18 +81,14 @@ public class AudioSettings : MonoBehaviour
         }
     }
 
-    public void SaveSettings()
+    public void EnableSettingsMenu()
     {
-        PlayerPrefs.SetFloat("MusicVol", currentVolume);
-        PlayerPrefs.SetInt("MutedBool", (muted ? 1: 0));
-    }
-
-    public void LoadSettings()
-    {
-        // Load the previous audio level
-        musicMixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVol"));
-
-        // Load whether it was muted bool
-        muted = (PlayerPrefs.GetInt("MutedBool") != 0);
+        if (settingsMenu.activeInHierarchy == false)
+        {
+            settingsMenu.SetActive(true);
+        } else if (settingsMenu.activeInHierarchy == true)
+        {
+            settingsMenu.SetActive(false);
+        }
     }
 }
