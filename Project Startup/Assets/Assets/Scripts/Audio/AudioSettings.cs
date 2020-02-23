5b8dd09ea0517b93e2a0aa64b21f700c64365201
Avery <averyfukawa@gmail.com>
2020-02-23 09:51:15 +0100

    Intro music now plays, Mute saving now works, saving is now more efficient

--- Project Startup/Assets/Assets/Scripts/Audio/AudioSettings.cs
+++ Project Startup/Assets/Assets/Scripts/Audio/AudioSettings.cs
@@ -17,13 +17,14 @@ public class AudioSettings : MonoBehaviour
     
     private void Start()
     {
-        Debug.Log("Current Muted bool value: " + muted);
+        // Debug.Log("Current Muted bool value: " + muted);
+        // Debug.Log(PlayerPrefs.GetInt("MutedBool") + " loaded MutedBool");
+        
         // Load the previous audio level, and set the saved level on in the music mixer
         currentVolume = PlayerPrefs.GetFloat("MusicVol");
         musicMixer.SetFloat("MusicVolume", currentVolume);
         volumeSlider.value = PlayerPrefs.GetFloat("SliderPos");
         
-        Debug.Log(PlayerPrefs.GetInt("MutedBool") + " loaded MutedBool");
         // Load whether it was muted bool
         if (PlayerPrefs.GetInt("MutedBool") == 0)
         {
@@ -37,32 +38,19 @@ public class AudioSettings : MonoBehaviour
             musicMixer.SetFloat("MusicVolume", -80.0f);
         }
     }
-    
-    private void Update()
-    {
-        Debug.Log("Current muted bool value: " + muted);
-        Debug.Log("saved MutedBool value: " + PlayerPrefs.GetInt("MutedBool"));
-        if (!enabled) return;
-        if (!muted)
-        {
-            PlayerPrefs.SetFloat("SliderPos", volumeSlider.value);
-            PlayerPrefs.SetFloat("MusicVol", currentVolume);
-        }
-        
-        if (muted == false)
-            PlayerPrefs.SetInt("MutedBool", 0);
-        else if (muted == true)
-            PlayerPrefs.SetInt("MutedBool", 1);
-        
-        //Debug.Log(PlayerPrefs.GetInt("MutedBool") + " Saved bool.\n0 is not muted, 1 is muted");
-    }
-    
+
     public void SetVolume(float volume)
     {
         if (!muted)
         {
             musicMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
             currentVolume = Mathf.Log10(volume) * 20;
+            
+            // Saving the values
+            PlayerPrefs.SetFloat("SliderPos", volumeSlider.value);
+            PlayerPrefs.SetFloat("MusicVol", currentVolume);
+            
+            // Debug.Log("Saved Music, Slider.pos");
         }
     }
 
@@ -73,20 +61,26 @@ public class AudioSettings : MonoBehaviour
             // Enable the music
             muted = false;
             musicMixer.SetFloat("MusicVolume", currentVolume);
+            PlayerPrefs.SetInt("MutedBool", 0);
+            
+            // Debug.Log("Saved bool false");
         } else if (!muted)
         {
             // Disable the music
             muted = true;
             musicMixer.SetFloat("MusicVolume", -80.0f);
+            PlayerPrefs.SetInt("MutedBool", 1);
+            
+            // Debug.Log("Saved bool true");
         }
     }
 
     public void EnableSettingsMenu()
     {
-        if (settingsMenu.activeInHierarchy == false)
+        if (!settingsMenu.activeInHierarchy)
         {
             settingsMenu.SetActive(true);
-        } else if (settingsMenu.activeInHierarchy == true)
+        } else if (settingsMenu.activeInHierarchy)
         {
             settingsMenu.SetActive(false);
         }
