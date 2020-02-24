fc4416c5c0162d5096b377e6b132979a14f041ff
Sven <Sven.Sackmann@web.de>
2020-02-24 18:37:58 +0100

    3/6 units implemented
    sorry for the late push T_T

---
+++ Project Startup/Assets/Assets/Scripts/Characters/Mighteus.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mighteus : Character
+{
+    private float smiteValue = 0;
+    public override void Update()
+    {
+        if (!isDead && gameManager.fightRunning)
+        {
+            Rotate();
+        
+            if (!fighting)
+            {
+                Move();
+                if (TargetInRange(aggroTarget))
+                {
+                    fighting = true;
+                }
+            }
+
+            if (fighting)
+            {
+                // check if aggroTarget TargetInRange, if yes, check attackCooldown against attack speed if yes Attack, else increase by deltatime, else make fighting false
+                if (TargetInRange(aggroTarget))
+                {
+                    if (isUpgraded)
+                    {
+                        if (attackCooldownValue >= attackCooldown)
+                        {
+                            attackCooldownValue = 0;
+                            Attack();
+                        }
+                        else
+                        {
+                            attackCooldownValue += Time.deltaTime;
+                        }
+                    }
+                    else
+                    {
+                        if (attackCooldownValue >= attackCooldown*2)
+                        {
+                            attackCooldownValue = 0;
+                            Attack();
+                        }
+                        else
+                        {
+                            attackCooldownValue += Time.deltaTime;
+                        }
+                    }
+                }
+                else
+                {
+                    fighting = false;
+                }
+            }
+        }
+    }
+
+    public override bool Damage(float amount)
+    {
+        smiteValue += defense / 3;
+        return base.Damage(amount);
+    }
+
+    public override void Attack()
+    {
+        StartCoroutine(FakeAttackAnimation(.2f));
+        // deal damage to the target mitigated by defense
+        if (aggroTarget.Damage(attackDamage+smiteValue - aggroTarget.defense))
+        {
+            // upgrade would go here, the following is placeholder
+            if (!isUpgraded)
+            {
+                Upgrade();
+            }
+        }
+
+        smiteValue = 0;
+    }
+
+    public override void Upgrade()
+    {
+        isUpgraded = true;
+        transform.LeanScale(new Vector3(1.3f, 1.3f, 1.3f), 0.3f);
+    }
+}
