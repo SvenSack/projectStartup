a51174f0c2a6822c63550d13c040ca0998fe3a7f
Sven <Sven.Sackmann@web.de>
2020-03-01 13:13:04 +0100

    Renaming and started work on teamManager

--- Project Startup/Assets/Assets/Scripts/TeamManager.cs
+++ Project Startup/Assets/Assets/Scripts/TeamManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 
@@ -7,6 +9,8 @@ public class TeamManager : MonoBehaviour
 {
     [HideInInspector] public Character[] yourTeam = new Character[3]; // your currently placed units
     [HideInInspector] public Character[] enemyTeam = new Character[3]; // the units on the enemy team
+    public GameObject[] yourTileRows;
+    public GameObject[] enemyTileRows;
 
     public TextMeshProUGUI unitCount;
     // Start is called before the first frame update
@@ -124,5 +128,32 @@ public class TeamManager : MonoBehaviour
         yield return new WaitForSeconds(0.3f);
         unitCount.color = new Color(50f/255f, 50f/255f, 50f/255f);
         unitCount.transform.LeanScale(new Vector3(1, 1, 1), .1f);
+    }
+
+    public void ReadTeamFile()
+    {
+        Tile[] tilesRow1 = enemyTileRows[0].GetComponentsInChildren<Tile>();
+        Tile[] tilesRow2 = enemyTileRows[1].GetComponentsInChildren<Tile>();
+        Tile[] tilesRow3 = enemyTileRows[2].GetComponentsInChildren<Tile>();
+        Tile[][] tileRows = new Tile[][]{tilesRow1,tilesRow2,tilesRow3};
+        string json = File.ReadAllText(Application.dataPath + "/saveFile.json");
+        TeamData teamData = JsonUtility.FromJson<TeamData>(json);
+        int[][] data = teamData.data;
+        for (int i = 0; i < data.Length; i++)
+        {
+            
+        }
+    }
+
+    public void WriteTeamFile()
+    {
+        TeamData dat = new TeamData();
+        string jsonData = JsonUtility.ToJson(dat);
+        //File.WriteAllText(Application.dataPath + "/saveFile.json", jsonData);
+    }
+    
+    private class TeamData
+    {
+        public int[][] data = new int[3][]{new []{0,2,3}, new []{1,1,1}, new []{2,2,4}};
     }
 }
