bf3d1e6ef9bc8ef81f20cdeab762497e48fdc32c
ThirzaL <60218577+ThirzaL@users.noreply.github.com>
2020-03-01 13:22:20 +0100

    Shader updated

---
+++ Project Startup/Assets/Assets/Shader/Unlit 1.hlsl
@@ -0,0 +1,22 @@
+﻿void LWRPLightingFunction_float (float3 WorldPos, out float3 Direction, out float ShadowAttenuation, out float3 Color)
+{
+   #ifdef LIGHTWEIGHT_LIGHTING_INCLUDED
+   
+      //Actual light data from the pipeline
+      Light light = GetMainLight(GetShadowCoord(GetVertexPositionInputs(WorldPos)));
+      Direction = light.direction;
+	  ShadowAttenuation = light.shadowAttenuation;
+      Color = light.color;
+      
+      
+   #else
+   
+      //Hardcoded data, used for the preview shader inside the graph
+      //where light functions are not available
+      Direction = float3(-0.5, 0.5, -0.5);
+	  ShadowAttenuation = 0.4;
+      Color = float3(1, 1, 1);
+      
+      
+   #endif
+}
