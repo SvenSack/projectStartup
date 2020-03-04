11cac644341701cd41eb134c3ed14bf1293f0f47
Sven <Sven.Sackmann@web.de>
2020-03-04 20:36:07 +0100

    animations in 70%

--- Project Startup/Assets/Assets/Scripts/Characters/Swiper.cs
+++ Project Startup/Assets/Assets/Scripts/Characters/Swiper.cs
@@ -2,21 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Debug = System.Diagnostics.Debug;
 
 public class Swiper : Character
 {
     private int hitCount = 1;
     public float swipeRange = 3;
     public float swipeRadius = 90;
+    private Animator anim;
+
+    public override void Start()
+    {
+        base.Start();
+        anim = GetComponentInChildren<Animator>();
+    }
+    
+    public override void Update()
+    {
+        if (!isDead && gameManager.fightRunning)
+        {
+            Rotate();
+        
+            if (!fighting)
+            {
+                if (anim.GetBool("Walking") == false)
+                {
+                    anim.SetBool("Walking", true);
+                }
+                Move();
+                if (TargetInRange(aggroTarget))
+                {
+                    anim.SetBool("Walking", false);
+                    fighting = true;
+                }
+            }
+
+            if (fighting)
+            {
+                // check if aggroTarget TargetInRange, if yes, check attackCooldown against attack speed if yes Attack, else increase by deltatime, else make fighting false
+                if (TargetInRange(aggroTarget))
+                {
+                    if (attackCooldownValue >= attackCooldown)
+                    {
+                        attackCooldownValue = 0;
+                        Attack();
+                    }
+                    else
+                    {
+                        attackCooldownValue += Time.deltaTime;
+                    }
+                }
+                else
+                {
+                    anim.SetBool("Walking", true);
+                    fighting = false;
+                }
+            }
+        }
+    }
+
     public override void Attack()
     {
+        
         if (hitCount < 3 - Convert.ToInt32(isUpgraded))
         {
             // Play Hit SFX
             hitSfx.Play();
             
             hitCount++;
-            StartCoroutine(FakeAttackAnimation(.2f));
+            // StartCoroutine(FakeAttackAnimation(.2f));
+            anim.SetTrigger("Attack");
             if (attackDamage > aggroTarget.defense)
                 if (aggroTarget.Damage(attackDamage - aggroTarget.defense))
                 {
@@ -32,7 +87,8 @@ public class Swiper : Character
         {
             hitCount = 1;
 
-            StartCoroutine(FakeAttackAnimation2(.2f));
+            // StartCoroutine(FakeAttackAnimation2(.2f));
+            anim.SetTrigger("Swipe");
             if(isOnYourTeam)
                 foreach (var unit in teamManager.enemyTeam)
                 {
